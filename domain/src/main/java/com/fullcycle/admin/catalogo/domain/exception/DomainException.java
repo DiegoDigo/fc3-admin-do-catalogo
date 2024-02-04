7d777092cae3ccdf9fb7974420da8bbb8dfb0620
Diego Domingos Delmiro <di3g0d0ming05@gmail.com>
2024-02-04 11:08:43 -0300

    fix: refatorando a stacktrace da exception

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/exception/DomainException.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/exception/DomainException.java
@@ -4,20 +4,24 @@ import com.fullcycle.admin.catalogo.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStacktraceException {
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErrors) {
-        super("", null, true, false);
+    private DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
+    }
+
+    public static DomainException with(final List<Error> anErros) {
+        return new DomainException("", anErros);
+    }
+
+    public static DomainException with(final Error anError) {
+        return new DomainException(anError.message(), List.of(anError));
     }
 
     public List<Error> getErrors() {
         return errors;
-    }
-
-    public static DomainException with(final List<Error> anErros) {
-        return new DomainException(anErros);
     }
 }
