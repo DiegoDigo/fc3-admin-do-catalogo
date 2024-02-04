49d8cb26641d5d4380491715e599a831e7b5aafd
Diego Domingos Delmiro <di3g0d0ming05@gmail.com>
2024-02-04 10:14:47 -0300

    commit inicial

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -0,0 +1,74 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import com.fullcycle.admin.catalogo.domain.AggregateRoot;
+
+import java.time.Instant;
+
+public class Category extends AggregateRoot<CategoryID> {
+
+    private String name;
+    private String description;
+    private boolean isActive;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    public CategoryID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+    private Category(
+            final CategoryID anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreatedAt,
+            final Instant aUpdatedAt,
+            final Instant aDeletedAt) {
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.isActive = isActive;
+        this.createdAt = aCreatedAt;
+        this.updatedAt = aUpdatedAt;
+        this.deletedAt = aDeletedAt;
+    }
+
+    public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
+        final var id = CategoryID.unique();
+        final var now = Instant.now();
+        return new Category(
+                id,
+                aName,
+                aDescription,
+                isActive,
+                now,
+                now,
+                null
+        );
+    }
+}
